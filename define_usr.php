704d9eeafb2d57375493b26c7838967b09539627
aubry_j <aubry_j@epitech.net>
2008-02-19 16:20:40 +0000

    Modification concernant l'utilisateur

--- define_usr.php
+++ define_usr.php
@@ -4,27 +4,28 @@
 ** Define user : name for xml
 */
 
-define('USR_XML_CREATE_BEGIN', '<create>');
-define('USR_XML_CREATE_END', '</create>');
-define('USR_XML_CONNECT_BEGIN', '<connect>');
-define('USR_XML_CONNECT_END', '</connect>');
+define('USR_CREATE_BEGIN', '<create>');
+define('USR_CREATE_END', '</create>');
+define('USR_CONNECT_BEGIN', '<connect>');
+define('USR_CONNECT_END', '</connect>');
 
-define('USR_XML_FIELD_LOGIN', '<field_login>%s</field_login>');
-define('USR_XML_FIELD_PASSWD', '<field_passwd>%s</field_passwd>');
-define('USR_XML_FIELD_REPASSWD', '<field_repasswd>%s</field_repasswd>');
-define('USR_XML_FIELD_EMAIL', '<field_email>%s</field_email>');
+define('USR_FIELD_LOGIN', '<field_login>%s</field_login>');
+define('USR_FIELD_PASSWD', '<field_passwd>%s</field_passwd>');
+define('USR_FIELD_REPASSWD', '<field_repasswd>%s</field_repasswd>');
+define('USR_FIELD_EMAIL', '<field_email>%s</field_email>');
 
 define('USR_XML_LOGIN', '<login>%s</login>');
 define('USR_XML_PASSWD', '<passwd>%s</passwd>');
 define('USR_XML_EMAIL', '<email>%s</email>');
 
-define('USR_XML_ERROR', '<error>%s</error>');
-define('USR_XML_ERROR_LOGIN', 'error : login already exist');
-define('USR_XML_ERROR_EMAIL', 'error : email invalid');
-define('USR_XML_ERROR_PASSWD_NOTFOUND', 'error: password isn\'t found.');
+define('USR_ERROR', '<error>%s</error>');
+define('USR_ERROR_LOGIN', 'error : wrong login');
+define('USR_ERROR_EMAIL', 'error : wrong email');
+define('USR_ERROR_PASSWD', 'error: password not found');
 
-define('USR_XML_MESG', '<mesg>%s</mesg>');
-define('USR_XML_MESG_OK', 'congratulation, user create');
+define('USR_MESG', '<mesg>%s</mesg>');
+define('USR_MESG_CREATE_OK', 'congratulation, user create');
+define('USR_MESG_CONNECT_OK', 'congratulation, you are login to %s');
 
 define('USR_REGEX_EMAIL', '^[a-zA-Z0-9\_\-\.]+\@+[a-zA-Z0-9\_\-\.]+\.[a-zA-Z0-9]{2,4}$');
 
@@ -43,17 +44,17 @@ define('USR_POST_EMAIL', 'usremail');
 
 define('USR_SQL_SELECT_LOGIN',
        'SELECT usr_login
-	FROM tw_usr
-	WHERE usr_login = \'%s\';');
+	    FROM tw_usr
+	    WHERE usr_login = \'%s\';');
 
 define('USR_SQL_SELECT_PASSWD',
        'SELECT usr_passwd
-	FROM tw_usr
-	WHERE usr_login = \'%s\';');
+	    FROM tw_usr
+	    WHERE usr_login = \'%s\';');
 
 /*
 ** Define user : sql add request
-*/
+
 
 define('USR_SQL_ADD_TABLE_USR',
        'INSERT INTO tw_usr
@@ -67,9 +68,9 @@ define('USR_SQL_ADD_TABLE_LOCATION',
        'INSERT INTO tw_location
 	VALUES(0, AAA, AAA);');
 
-/*
+
 ** Define user : sql update request
-*/
+
 
 define('USR_SQL_UPDATE_TABLE_USR',
        'UPDATE tw_usr
@@ -91,5 +92,5 @@ define('USR_SQL_UPDATE_TABLE_LOCATION',
 	WHERE location_id = (SELECT usr_profil_id
 			     FROM tw_usr
 			     WHERE usr_login = \'%s\');');
-
+*/
 ?>
--- function_usr.php
+++ function_usr.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once('./define_usr.php');
 
 function usr_login_check()
@@ -10,30 +9,54 @@ function usr_login_check()
   return (0);
 }
 
+function usr_passwd_check()
+{
+  $test = mysql_query(sprintf(USR_SQL_SELECT_LOGIN, $_POST[USR_POST_LOGIN]));
+  $passwd = @mysql_num_rows($test);
+  if (strcmp(sha1($_POST[USR_POST_PASSWD]), $passwd) == 0)
+    return (1);
+  return (0);
+}
+
+function usr_repasswd_check()
+{
+  if (strcmp($_POST[USR_POST_PASSWD], $_POST[USR_POST_REPASSWD]) == 0)
+	return (1);
+  return (0);
+}
+
 function usr_email_check()
 {
-  $email = ereg(USR_REGEX_EMAIL, $_POST[USR_POST_EMAIL]);
-  if ($email == FALSE)
+  if (ereg(USR_REGEX_EMAIL, $_POST[USR_POST_EMAIL]) != FALSE)
     return (1);
   return (0);
 }
 
 function usr_add()
 {
-  $test = usr_login_check();
-  if ($test)
-    {
-      $doc = sprintf(USR_XML_DOC, sprintf(USR_XML_ERROR, USR_XML_ERROR_LOGIN));
-      return ($doc);
-    }
-  $test = usr_email_check();
-  if ($test)
-    {
-      $doc = sprintf(USR_XML_DOC, sprintf(USR_XML_ERROR, USR_XML_ERROR_EMAIL));
-      return ($doc);
-    }
-  $doc = sprintf(USR_XML_DOC, sprintf(USR_XML_MESG, USR_XML_MESG_OK));
-  return ($doc);
+  $login = usr_login_check();
+  if ($login)
+	printf(USR_ERROR, USR_ERROR_LOGIN);
+  $passwd = usr_repasswd_check();
+  if (!$login && !$passwd)
+    printf(USR_ERROR, USR_ERROR_REPASSWD);
+  $email= usr_email_check();
+  if (!$login && $passwd && !$email)
+    printf(USR_ERROR, USR_ERROR_EMAIL);
+  if (!$login && $passwd && $email)
+    printf(USR_MESG, USR_MESG_CREATE_OK);
+}
+
+function usr_connect()
+{
+  $login = usr_login_check();
+  $passwd = usr_passwd_check();
+  if (!login)
+	printf(USR_ERROR, USR_ERROR_LOGIN);
+  if (!passwd)
+	printf(USR_ERROR, USR_ERROR_PASSWD);
+  if (login && passwd)
+    printf(USR_MESG, USR_MESG_CONNECT_OK);
 }
 
 ?>
